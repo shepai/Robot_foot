1ce714b472a6c766e424f0f5ae9a901b9973174b
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2026-04-09 13:35:14 +0100

    sensor

--- Mujuco/test_presstip_foot.py
+++ Mujuco/test_presstip_foot.py
@@ -23,7 +23,7 @@ def show_heatmap(grid):
 import time
 
 # Load model
-model = mj.MjModel.from_xml_path("C:/Users/dexte/Documents/GitHub/Robot_foot/Mujuco/presstip_foot.xml")
+model = mj.MjModel.from_xml_path("C:/Users/dexte/Documents/GitHub/Robot_foot/Mujuco/ball_presstip_env.xml")
 data = mj.MjData(model)
 
 # --- Pad setup ---
